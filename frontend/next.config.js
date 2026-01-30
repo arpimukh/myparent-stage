6559355658a055b79b7d9f4c5e6ab97096869084
arpimukh <arpi_7382000@yahoo.com>
2026-01-30 18:43:27 +0530

    next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,7 @@ module.exports = {
     return [
       {
         // 1. The path users will call on your frontend
-        source: '/api/v1/:path*', 
+        source: '/api/:path*', 
         // 2. The internal URL of your backend deployment
         destination: 'https://myparent-stage-a2zc-avcxfo3zw-arpimukhs-projects.vercel.app/:path*', 
       },
