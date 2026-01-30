4013db48fc786addb2231819260ce6b9a2a0017a
arpimukh <arpi_7382000@yahoo.com>
2026-01-30 20:28:35 +0530

    updated destination

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,35 +1,32 @@
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        // Apply CORS headers to all API routes
-        source: "/api/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" }, // Replace with your domain in production
-          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-          { 
-            key: "Access-Control-Allow-Headers", 
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" 
-          },
-        ],
-      },
-    ];
-  },
+export async function headers() {
+  return [
+    {
+      // Apply CORS headers to all API routes
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+        { key: "Access-Control-Allow-Origin", value: "*" }, // Replace with your domain in production
+        { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+        },
+      ],
+    },
+  ];
+}
+export async function rewrites() {
+  return [
+    {
+      // 1. The path users will call on your frontend
+      source: '/api/:path*',
+      // 2. The internal URL of your backend deployment
+      destination: 'https://myparent-stage-a2zc-avcxfo3zw-arpimukhs-projects.vercel.app/api/:path*',
+    },
+  ];
+}
 
-  async rewrites() {
-    return [
-      {
-        // 1. The path users will call on your frontend
-        source: '/api/:path*', 
-        // 2. The internal URL of your backend deployment
-        destination: 'https://myparent-stage-a2zc-avcxfo3zw-arpimukhs-projects.vercel.app/api/:path*', 
-      },
-    ];
-  },
-};
-
-module.exports = nextConfig;
+//module.exports = nextConfig;
 
 
 // frontend/next.config.js
