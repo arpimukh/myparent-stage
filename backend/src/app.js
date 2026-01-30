80a4a81505961765c5695be08d56ac82e4b65241
arpimukh <arpi_7382000@yahoo.com>
2026-01-30 21:11:15 +0530

    updated origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-
+app.use(cors({
+  origin: 'https://myparent-stage-3by24ilen-arpimukhs-projects.vercel.app',
+ credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 module.exports = app;
