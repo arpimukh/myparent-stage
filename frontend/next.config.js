a40706da2aebaf67778d347c4634d671c297fedf
powerpuff-arpita <arpi_7382000@yahoo.com>
2026-02-01 01:13:01 +0530

    eofbye

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,21 +1,21 @@
 const nextConfig = {
-  // async headers() {
-  //   return [
-  //     {
-  //       // Apply CORS headers to all API routes
-  //       source: "/api/:path*",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Credentials", value: "true" },
-  //         { key: "Access-Control-Allow-Origin", value: "https://myparent-stage-a2zc.vercel.app" }, // Replace with your domain in production
-  //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
-  //         { 
-  //           key: "Access-Control-Allow-Headers", 
-  //           value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" 
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
+  //  async headers() {
+  //    return [
+  //      {
+  // //       // Apply CORS headers to all API routes
+  //        source: "/v1/:path*",
+  //        headers: [
+  //          { key: "Access-Control-Allow-Credentials", value: "true" },
+  //          { key: "Access-Control-Allow-Origin", value: "https://myparent-stage-ampu.vercel.app" }, // Replace with your domain in production
+  //          { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
+  //          { 
+  //            key: "Access-Control-Allow-Headers", 
+  //            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization" 
+  //          },
+  //        ],
+  //      },
+  //    ];
+  //  },
 
   async rewrites() {
     return [
@@ -23,7 +23,7 @@ const nextConfig = {
         // 1. The path users will call on your frontend
         source: '/v1/:path*', 
         // 2. The internal URL of your backend deployment
-        destination: 'https://myparent-stage-a2zc.vercel.app/:path*', 
+        destination: 'https://myparent-stage-ampu.vercel.app/:path*', 
       },
     ];
   },
